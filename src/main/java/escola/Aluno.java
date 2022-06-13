4b43727176325418fc9ef10241ae134005aea59e
Lucas Galvao <lucas.engcomp@outlook.com>
2022-06-13 17:09:09 -0300

    Criado o padrão fábrica de objeto aluno e telefone

--- src/main/java/escola/Aluno.java
+++ src/main/java/escola/Aluno.java
@@ -9,8 +9,48 @@ public class Aluno {
 	private String nome;
 	private Email email;
 	private List<Telefone> telefones = new ArrayList<>();
-	
+		
+	public Aluno(Cpf cpf, String nome, Email email) {
+		super();
+		this.cpf = cpf;
+		this.nome = nome;
+		this.email = email;
+	}
+
 	public void adicionarTelefone(String ddd, String numero) {
 		this.telefones.add(new Telefone(ddd, numero));
 	}
+
+	public Cpf getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(Cpf cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Email getEmail() {
+		return email;
+	}
+
+	public void setEmail(Email email) {
+		this.email = email;
+	}
+
+	public List<Telefone> getTelefones() {
+		return telefones;
+	}
+	
+	public static void main(String[] args) {
+		Aluno a = new Aluno(new Cpf(""), "Lucas Galvao", new Email("email@gmail.com"));
+	}
+	
 }
