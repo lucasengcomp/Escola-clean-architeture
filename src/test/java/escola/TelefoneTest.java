9fffd91d332ab11d445ac6ddc2fd45f9b7dff3a2
Lucas Galvao <lucas.engcomp@outlook.com>
2022-06-13 16:47:31 -0300

    Criado teste de telefone

---
+++ src/test/java/escola/TelefoneTest.java
@@ -0,0 +1,24 @@
+package escola;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TelefoneTest {
+
+	@Test
+	void naoDeveriaCriarTelefoneComDDDsInvalidos() {
+		assertThrows(IllegalArgumentException.class, () -> new Telefone(null, "123456789"));
+		assertThrows(IllegalArgumentException.class, () -> new Telefone("", "123456789"));
+		assertThrows(IllegalArgumentException.class, () -> new Telefone("1", "123456789"));
+	}
+	
+	@Test
+	void deveriaPermitirCriarTelefoneComDDDENumeroValidos() {
+		String ddd = "11";
+		String numero = "123456789";
+		Telefone telefone = new Telefone(ddd, numero);
+		assertEquals(ddd, telefone.getDdd());
+		assertEquals(numero, telefone.getNumero());
+	}
+}
