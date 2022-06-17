6e37ddedd3223365fc2a6e5c7dcd9292f786850d
Lucas Galvao <lucas.engcomp@outlook.com>
2022-06-16 21:21:17 -0300

    Adicionado teste de matrícula de Aluno

---
+++ src/test/java/br/com/alura/aluno/MatricularAlunoTest.java
@@ -0,0 +1,34 @@
+package br.com.alura.aluno;
+
+import br.com.alura.escola.aplicacao.aluno.matricular.MatricularAluno;
+import br.com.alura.escola.aplicacao.aluno.matricular.MatricularAlunoDto;
+import br.com.alura.escola.dominio.aluno.Aluno;
+import br.com.alura.escola.dominio.aluno.Cpf;
+import br.com.alura.escola.infra.aluno.RepositorioDeAlunosEmMemoria;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MatricularAlunoTest {
+
+    @Test
+    void alunoDeveriaSerPersistido() {
+        // MOCK -> Mockito
+        RepositorioDeAlunosEmMemoria repositorio = new RepositorioDeAlunosEmMemoria();
+        MatricularAluno useCase = new MatricularAluno(repositorio);
+
+        MatricularAlunoDto dados = new MatricularAlunoDto(
+                "Fulano",
+                "123.456.789-00",
+                "fulano@email.com");
+        useCase.executa(dados);
+
+        Aluno encontrado = repositorio.buscarPorCpf(
+                new Cpf("123.456.789-00"));
+
+        assertEquals("Fulano", encontrado.getNome());
+        assertEquals("123.456.789-00", encontrado.getCpf());
+        assertEquals("fulano@email.com", encontrado.getEmail());
+    }
+
+}
